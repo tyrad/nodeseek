f819ce72be1e2fd10e4fd5a2704bcfdf1ae3fa6e
mist <tyradccc@gmail.com>
2026-04-30 15:02:41 +0800

    refactor: move account response into core domain

--- nodeseek/Core/Domain/NodeSeekModels.swift
+++ nodeseek/Core/Domain/NodeSeekModels.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+struct AccountResponse: Equatable, Sendable {
+    let displayName: String
+    let isLoggedIn: Bool
+    let avatarURL: URL?
+    let profileURL: URL?
+    let stats: [String]
+
+    init(
+        displayName: String,
+        isLoggedIn: Bool,
+        avatarURL: URL? = nil,
+        profileURL: URL? = nil,
+        stats: [String] = []
+    ) {
+        self.displayName = displayName
+        self.isLoggedIn = isLoggedIn
+        self.avatarURL = avatarURL
+        self.profileURL = profileURL
+        self.stats = stats
+    }
+}
+
 struct PostSummary: Equatable, Sendable {
     let id: String
     let title: String
--- nodeseek/Features/Account/AccountEntity.swift
+++ nodeseek/Features/Account/AccountEntity.swift
@@ -10,25 +10,3 @@ import Foundation
 struct AccountRequest {
     let refresh: Bool
 }
-
-struct AccountResponse: Equatable, Sendable {
-    let displayName: String
-    let isLoggedIn: Bool
-    let avatarURL: URL?
-    let profileURL: URL?
-    let stats: [String]
-
-    init(
-        displayName: String,
-        isLoggedIn: Bool,
-        avatarURL: URL? = nil,
-        profileURL: URL? = nil,
-        stats: [String] = []
-    ) {
-        self.displayName = displayName
-        self.isLoggedIn = isLoggedIn
-        self.avatarURL = avatarURL
-        self.profileURL = profileURL
-        self.stats = stats
-    }
-}
